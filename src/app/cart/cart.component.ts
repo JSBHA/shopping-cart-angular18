811c8329d4d133f95144f824875cd1cdc7503be3
JSBHA <jhoan-071995@hotmail.com>
2024-09-29 19:32:41 -0500

    Se crean las funciones para

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { CartItemComponent } from './ui/cart-item/cart-item.component';
 import { cartStateService } from '../shared/data-access/cart-state.service';
+import { ProductItemcart } from '../shared/interfaces/product.interface';
 
 @Component({
   selector: 'app-cart',
@@ -16,7 +17,20 @@ export  default class CartComponent {
 
   onRemove(id: number) {
     this.state.remove(id)
+  }
 
+  onIncrease(product: ProductItemcart) {
+    this.state.udpate({
+      product: product.product,
+      quantity: product.quantity +1,
+    });
+  }
+
+  onDecrease(product: ProductItemcart) {
+    this.state.udpate({
+      ...product,
+      quantity: product.quantity -1,
+    });
   }
 
 
